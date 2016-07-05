6831ccf59d417f99332db4d9e8e3b864711dfca7
Fabio dos Santos Barbosa <fabio.barbosa@gmail.com>
2016-07-05 18:23:32 -0300

    v1.0.0

---
+++ factories.js
@@ -0,0 +1,10 @@
+'use strict';
+/*global URL_SISTEMA */
+angular.module('stn.modulo-factories',[])
+
+  .factory('LoginSvc', function($resource){ 
+	return $resource(URL_SISTEMA +'/rest/Usuario');
+	})  
+	.factory('logoutSvc', function($resource){ //servico para logout
+		return $resource(URL_SISTEMA +'/rest/Usuario/Sair'  );
+	})
